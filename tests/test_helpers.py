b805380cd66b890b18009420edf3b26bd39770fb
Mike <Mikea54@gmail.com>
2025-05-20 13:35:42 -0600

    Add tests for format_date

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,5 @@
 import datetime
-from utils.helpers import calculate_leave_days
+from utils.helpers import calculate_leave_days, format_date
 
 
 def test_calculate_leave_days_exclude_weekends():
@@ -18,3 +18,14 @@ def test_calculate_leave_days_invalid_range():
     start = datetime.date(2024, 1, 7)
     end = datetime.date(2024, 1, 1)
     assert calculate_leave_days(start, end) == 0
+
+
+def test_format_date_with_datetime():
+    dt = datetime.datetime(2024, 1, 1, 12, 34, 56)
+    assert format_date(dt) == "2024-01-01 12:34:56"
+
+
+def test_format_date_with_date():
+    d = datetime.date(2024, 1, 1)
+    assert format_date(d) == "2024-01-01"
+
