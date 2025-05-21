795aaa8b75a8cab22d2041b57173ee86882567a7
Mike <Mikea54@gmail.com>
2025-05-21 13:01:43 -0600

    Add upload folder config

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
 
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", os.path.join(BASE_DIR, 'static', 'uploads'))
